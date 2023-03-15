c27c5d2f8740580c271b5a439e80578c37dc0159
xifan0614 <xifan0614@gmail.com>
2023-03-15 15:38:14 +0800

    unity work commit

---
+++ GIT/Assets/Scripts/xifan.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xifan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("2023/3/15 work");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
