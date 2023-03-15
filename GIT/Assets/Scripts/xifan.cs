352aebad4f09f46bfee593b2e57deda1f81e4935
tnoy44416 <tnoy44416@gmail.com>
2023-03-15 15:44:59 +0800

    work

--- GIT/Assets/Scripts/xifan.cs
+++ GIT/Assets/Scripts/xifan.cs
@@ -7,7 +7,9 @@ public class xifan : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("2023/3/15 work");
+        print("2023/3/15 work" +
+            "2023/315 work" +
+            "");
     }
 
     // Update is called once per frame
